6ad0d2b882004c21bc96d57b920479c1f89475fe
alex <alex@crunchy.com>
2013-10-15 18:23:45 -0700

    update documentation

--- doxygen/html/search/all_65.js
+++ doxygen/html/search/all_65.js
@@ -6,6 +6,7 @@ var searchData=
   ['evwsconn_5fclose_5fcb',['evwsconn_close_cb',['../evws_8h.html#a3ff2812905f167acfd273b7b9767b0a5',1,'evws.h']]],
   ['evwsconn_5ferror_5fcb',['evwsconn_error_cb',['../evws_8h.html#a303aa5d176d47e8a95e0f4470a0a4c3e',1,'evws.h']]],
   ['evwsconn_5ffree',['evwsconn_free',['../evws_8h.html#ac20903ad7c62ace704b5963ede4a02ed',1,'evws.h']]],
+  ['evwsconn_5fget_5fsubprotocol',['evwsconn_get_subprotocol',['../evws_8h.html#ace5173ab7bd90c471bc0b86610f2cf9a',1,'evws.h']]],
   ['evwsconn_5fmessage_5fcb',['evwsconn_message_cb',['../evws_8h.html#a65aff176bd81deecb4d6c45e6933a292',1,'evws.h']]],
   ['evwsconn_5fsend_5fclose',['evwsconn_send_close',['../evws_8h.html#a62c804f46c3c62d0550edd58693f55ce',1,'evws.h']]],
   ['evwsconn_5fsend_5fmessage',['evwsconn_send_message',['../evws_8h.html#aa35e924805a31e08cfa5f68bc9dcb0ec',1,'evws.h']]],
@@ -13,8 +14,8 @@ var searchData=
   ['evwsconnlistener_5fcb',['evwsconnlistener_cb',['../wslistener_8h.html#ad631dd71a42f892de2c4e7514c342162',1,'wslistener.h']]],
   ['evwsconnlistener_5ferrorcb',['evwsconnlistener_errorcb',['../wslistener_8h.html#ab7e8257cc8ab648283d2f5365c0f36c4',1,'wslistener.h']]],
   ['evwsconnlistener_5ffree',['evwsconnlistener_free',['../wslistener_8h.html#adee96c4899f8a1d1dbc459a310558332',1,'wslistener.h']]],
-  ['evwsconnlistener_5fnew',['evwsconnlistener_new',['../wslistener_8h.html#a88408b767fe3ee23adeac3087f40c3e8',1,'wslistener.h']]],
-  ['evwsconnlistener_5fnew_5fbind',['evwsconnlistener_new_bind',['../wslistener_8h.html#a7038518ef85155e116fc1a5b9f1df4c3',1,'wslistener.h']]],
+  ['evwsconnlistener_5fnew',['evwsconnlistener_new',['../wslistener_8h.html#ad5483c5508864cb3b13cb9b07d7e97d8',1,'wslistener.h']]],
+  ['evwsconnlistener_5fnew_5fbind',['evwsconnlistener_new_bind',['../wslistener_8h.html#ac5b55d549955f8c2168a4827a86c9728',1,'wslistener.h']]],
   ['evwsconnlistener_5fset_5fcb',['evwsconnlistener_set_cb',['../wslistener_8h.html#adc2da60dcf51a76117f97c7549c07051',1,'wslistener.h']]],
   ['evwsconnlistener_5fset_5ferror_5fcb',['evwsconnlistener_set_error_cb',['../wslistener_8h.html#a7b36ea0d490009b1c7903b2d720abd63',1,'wslistener.h']]]
 ];
--- doxygen/html/search/functions_65.js
+++ doxygen/html/search/functions_65.js
@@ -2,12 +2,13 @@ var searchData=
 [
   ['evconnlistener_5fget_5fevconnlistener',['evconnlistener_get_evconnlistener',['../wslistener_8h.html#a43f711ea78a0246cf2d4ceb885d1b1b9',1,'wslistener.h']]],
   ['evwsconn_5ffree',['evwsconn_free',['../evws_8h.html#ac20903ad7c62ace704b5963ede4a02ed',1,'evws.h']]],
+  ['evwsconn_5fget_5fsubprotocol',['evwsconn_get_subprotocol',['../evws_8h.html#ace5173ab7bd90c471bc0b86610f2cf9a',1,'evws.h']]],
   ['evwsconn_5fsend_5fclose',['evwsconn_send_close',['../evws_8h.html#a62c804f46c3c62d0550edd58693f55ce',1,'evws.h']]],
   ['evwsconn_5fsend_5fmessage',['evwsconn_send_message',['../evws_8h.html#aa35e924805a31e08cfa5f68bc9dcb0ec',1,'evws.h']]],
   ['evwsconn_5fset_5fcbs',['evwsconn_set_cbs',['../evws_8h.html#ad7bf7f988d11cd81493f090768224c80',1,'evws.h']]],
   ['evwsconnlistener_5ffree',['evwsconnlistener_free',['../wslistener_8h.html#adee96c4899f8a1d1dbc459a310558332',1,'wslistener.h']]],
-  ['evwsconnlistener_5fnew',['evwsconnlistener_new',['../wslistener_8h.html#a88408b767fe3ee23adeac3087f40c3e8',1,'wslistener.h']]],
-  ['evwsconnlistener_5fnew_5fbind',['evwsconnlistener_new_bind',['../wslistener_8h.html#a7038518ef85155e116fc1a5b9f1df4c3',1,'wslistener.h']]],
+  ['evwsconnlistener_5fnew',['evwsconnlistener_new',['../wslistener_8h.html#ad5483c5508864cb3b13cb9b07d7e97d8',1,'wslistener.h']]],
+  ['evwsconnlistener_5fnew_5fbind',['evwsconnlistener_new_bind',['../wslistener_8h.html#ac5b55d549955f8c2168a4827a86c9728',1,'wslistener.h']]],
   ['evwsconnlistener_5fset_5fcb',['evwsconnlistener_set_cb',['../wslistener_8h.html#adc2da60dcf51a76117f97c7549c07051',1,'wslistener.h']]],
   ['evwsconnlistener_5fset_5ferror_5fcb',['evwsconnlistener_set_error_cb',['../wslistener_8h.html#a7b36ea0d490009b1c7903b2d720abd63',1,'wslistener.h']]]
 ];
